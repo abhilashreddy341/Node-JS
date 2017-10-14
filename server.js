900526deb574872fff931f3b8d2485c1373c3292
aggg6 <aggg6@mail.umkc.edu>
2017-10-14 15:57:41 -0500

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT||3000;
 var app = express();
 app.set('view engine','hbs');
 
@@ -22,9 +23,9 @@ app.use((req,res,next)=>{
   next();
 })
 
-app.use((req,res,next)=>{
-  res.render('maintainance.hbs');
-})
+// app.use((req,res,next)=>{
+//   res.render('maintainance.hbs');
+// })
 
 app.use(express.static(__dirname+'/public'));
 
@@ -49,4 +50,6 @@ app.get('/error',(req,res)=>{
     status: ' error occured',
   })
 })
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`Server is up on Port: ${port}`);
+});
